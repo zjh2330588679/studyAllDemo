0bee7e7e5651928f92c917b52422521e19e463ee
zhaijihan <zhaijihan@163.com>
2020-10-12 09:33:10 +0800

    second

--- jdk8Demo/src/main/java/com/zjh/JdkApplication.java
+++ jdk8Demo/src/main/java/com/zjh/JdkApplication.java
@@ -2,6 +2,7 @@ package com.zjh;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
@@ -11,8 +12,8 @@ import java.util.stream.Collectors;
 public class JdkApplication {
     public static void main(String[] args) {
         //foreach
-        /*Random random = new Random();
-        random.ints().limit(10).forEach(System.out::println);*/
+        Random random = new Random();
+        random.ints().limit(10).forEach(System.out::println);
         List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
         List<Integer> collect = numbers.stream().map(i -> i*i).collect(Collectors.toList());
         collect.forEach(System.out::println);
